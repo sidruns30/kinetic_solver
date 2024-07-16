a455bfe1a7c2c042a31646ccd08359cff111126c
Siddhant Solanki <siddhantsolanki@wireless-10-104-140-134.umd.edu>
2024-07-16 14:02:29 -0400

    Created and added util files

---
+++ util.cpp
@@ -0,0 +1,174 @@
+#include "util.hpp"
+
+/*
+    Return the magnitude of a vector
+*/
+double Mag(const array v)
+{
+    return sqrt(v[0]*v[0] + v[1]*v[1] + v[2]*v[2]);
+}
+
+/*
+    Return the magnitude of the x-y component of the vector
+*/
+double MagPerp(const array v)
+{
+    return sqrt(v[0]*v[0] + v[1]*v[1]);
+}
+
+/*
+    Return the dot product between vectors v1 and v2
+*/
+double Dot(const array v1, const array v2)
+{
+    return v1[0]*v2[0] + v1[1]*v2[1] + v1[2]*v2[2];
+}
+
+/*
+    Return the cross product between vectors v1 and v2
+    Result stored in v3
+*/
+array Cross(const array v1, const array v2)
+{
+    array v3 = {v2[2]*v1[1] - v2[1]*v1[2], 
+                v2[0]*v1[2] - v2[2]*v1[0],
+                v2[1]*v1[0] - v2[0]*v1[1]};
+    return v3;
+}
+
+/*
+    Return the unit vector of the input vector v
+    Result stored in vhat
+*/
+array Hat(const array v)
+{
+    const double mag_v = Mag(v);
+    array vhat = {v[0]/mag_v, v[1]/mag_v, v[2]/mag_v};
+    return vhat;
+}
+
+/*
+    Return the sign of x
+    Edgecase: returns 0 when x is 0
+*/
+double Sign(const double x)
+{
+    if (x > 0.) return 1.;
+    else if (x < 0.) return -1.;
+    else return 0.;
+}
+
+/*
+    Return the heavyside function x
+*/
+double Heavyside(const double x)
+{
+    if (x < 0.) return 0.; 
+    else if (x > 0.) return 1.;
+    else return 0.5;
+}
+
+/*
+    Print a message to the output stream
+*/
+void PrintMessage(std::string message)
+{
+    std::cout << message << std::endl;
+    return;
+}
+
+
+/*
+    Throw an exception
+*/
+void RaiseException(std::string message)
+{
+    std::cerr << "An exception has occurred." << std::endl;
+    throw std::runtime_error(message);
+    return;
+}
+
+
+/*
+    Print the components of a vector
+*/
+void PrintVector(const double v[3])
+{
+    std::cout << "[" << v[0] << ", " << v[1] << ", " << v[2] << "]" << std::endl;
+    return;
+}
+
+void PrintVector(const array &v)
+{
+    const size_t vec_size = v.size();
+    std::cout << "Vector shape is " << vec_size << std::endl;
+    if (vec_size < 20)
+    {
+        std::cout << "[";
+        for (size_t ii=0; ii<vec_size; ii++)
+        {
+            std::cout << v[ii] << ", ";
+        }
+        std::cout << "]";
+    }
+    else
+    {
+        std::cout << "[";
+        for (size_t ii=0; ii<10; ii++)
+        {
+            std::cout << v[ii] << ", ";
+        }
+        std::cout << " ... ";
+        for (size_t ii=vec_size-10; ii<vec_size; ii++)
+        {
+            std::cout << v[ii] << ", ";
+        }
+        std::cout << "]";
+    }
+    std::cout << std::endl;
+    return;
+}
+
+/*
+    Print the components of a vector
+*/
+void PrintVector(const double v[3], std::string vecname)
+{
+    std::cout << "Vector: " << vecname << std::endl;
+    std::cout << "[" << v[0] << ", " << v[1] << ", " << v[2] << "]" << std::endl;
+    return;
+}
+
+/*
+    Print the elements of a vector for debugging purposes
+*/
+void PrintVector(const array v, std::string vecname)
+{
+    const size_t vec_size = v.size();
+    std::cout << "Vec: " << vecname << " shape is " << vec_size << std::endl;
+    if (vec_size < 20)
+    {
+        std::cout << "[";
+        for (size_t ii=0; ii<vec_size; ii++)
+        {
+            std::cout << v[ii] << ", ";
+        }
+        std::cout << "]";
+    }
+    else
+    {
+        std::cout << "[";
+        for (size_t ii=0; ii<10; ii++)
+        {
+            std::cout << v[ii] << ", ";
+        }
+        std::cout << " ... ";
+        for (size_t ii=vec_size-10; ii<vec_size; ii++)
+        {
+            std::cout << v[ii] << ", ";
+        }
+        std::cout << "]";
+    }
+    std::cout << std::endl;
+    return;
+}
---
+++ util.hpp
@@ -0,0 +1,35 @@
+#ifndef UTIL_H
+    #define UTIL_H (123456789)
+    #include "omp.h"
+    #include "stdlib.h"
+    #include "math.h"
+    #include "time.h"
+    #include <vector>
+    #include <stdio.h>
+    #include <iostream>
+    #include <stdexcept>
+    #include <string>
+    
+    const double PI = 3.14159265359;
+    const double E =  2.71828182846;
+
+    typedef std::vector<double> array;
+    typedef std::vector<std::vector<double>> array2D;
+    typedef std::vector<size_t> i_array;
+    typedef std::vector<std::vector<size_t>> i_array2D;
+
+    // Debugging
+    void PrintMessage(std::string message);
+    void RaiseException(std::string message);
+    void PrintVector(const double v[3]);
+    void PrintVector(const array &v);
+
+    // Vector operations
+    double Mag(const array v);
+    double MagPerp(const array v);
+    double Dot(const array v1, const array v2);
+    array Cross(const array v1, const array v2);
+    array Hat(const array v);
+    double Sign(const double x);
+    double Heavyside(const double x);
+#endif
